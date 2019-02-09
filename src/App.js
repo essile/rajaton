40e74e730340085094dc707d61526bf92432341f
esle <essi.lehtola@hotmail.com>
2019-02-09 08:27:40 +0200

    test requests ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const API_ADDRESS = 'http://localhost:5000';
 
 class App extends Component {
   state = {
@@ -9,20 +10,16 @@ class App extends Component {
 
   componentDidMount() {
 
-    // Call our fetch function below once the component mounts
     this.callBackendAPI()
       .then(res => this.setState({ data: res.test }))
       .catch(err => console.log(err));
   }
-  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
   callBackendAPI = async () => {
-    const response = await fetch('/api/hello');
+    const response = await fetch(API_ADDRESS + '/api/hello');
     const body = await response.json();
 
-    console.log(body);
-
     if (response.status !== 200) {
-      throw Error(body.message)
+      throw Error(body.message) 
     }
     return body;
   };
