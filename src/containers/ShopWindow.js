2f3032de9c48e9b2c90da8cd7af2dc250f14f599
esle <essi.lehtola@hotmail.com>
2019-04-18 08:45:45 +0300

    shopping cart component and modal started

--- src/containers/ShopWindow.js
+++ src/containers/ShopWindow.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Modal, Button } from 'react-bootstrap';
 import '../styles/ShopWindow.css'
+import ShoppingCart from './ShoppingCart';
 
 const BUTTON_TEXT_ADD = 'Add to cart';
-const BUTTON_TEXT_ADDED = 'Added to cart';
 const formatter = new Intl.NumberFormat('fi-EN', {
     style: 'currency',
     currency: 'EUR',
@@ -15,7 +15,8 @@ class ShopWindow extends Component {
 
     state = {
         stockProducts: [],
-        buttonTexts: []
+        buttonTexts: [],
+        showModal: false,
     };
 
     componentDidMount() {
@@ -34,65 +35,65 @@ class ShopWindow extends Component {
     }
 
     handleClick = (product, index) => {
-        if (this.state.buttonTexts[index] === BUTTON_TEXT_ADD) {
-            this.addToCart(product);
-            let copyOfButtonTexts = { ...this.state.buttonTexts };
-            copyOfButtonTexts[index] = BUTTON_TEXT_ADDED;
-            this.setState({ buttonTexts: copyOfButtonTexts });
-        } else {
-            this.removeFromCart(product);
-            let copyOfButtonTexts = { ...this.state.buttonTexts };
-            copyOfButtonTexts[index] = BUTTON_TEXT_ADD;
-            this.setState({ buttonTexts: copyOfButtonTexts });
-        }
+        this.setState({ showModal: true });
     }
 
-    addToCart = (product) => {
-        console.log('adding to cart', product);
-        // request to backend
-    }
-
-    removeFromCart = (product) => {
-        console.log('removing from cart', product);
-        // request to backend
+    handleClose = () => {
+        this.setState({ showModal: false });
     }
 
     render() {
         return (
-            <Row className='shopWindow' id='shop-window'>
-                {this.state.stockProducts.map((product, index) => {
-                    return <Col md={6} lg={4} key={index} style={{ padding: '2px' }}>
-                        <div className='centeredImage'>
-                            <img
-                                className='productIconSmall'
-                                alt={`${product.name} ${product.category}`}
-                                src={window.location.origin + '/' + product.image_url}
-                            />
-                        </div>
-                        <div className='centeredImage'>
-                            <span className='productNameText'>
-                                {product.name.toLowerCase()}
-                            </span>
-                            <br />
-                            <span className='categoryText'>
-                                {product.category.toLowerCase()}
-                            </span>
-                        </div>
-                        <div className='productPrice'>
-                            {formatter.format(product.price)}
-                        </div>
-                        <div style={{ textAlign: 'center' }}>
-                            <button
-                                className='addToCartButton'
-                                id={`button${index}`}
-                                onClick={() => this.handleClick(product, index)}
-                            >
-                                {this.state.buttonTexts[index].toUpperCase()}
-                            </button>
-                        </div>
-                    </Col>
-                })}
-            </Row>
+            <>
+                <Row className='shopWindow' id='shop-window'>
+                    {this.state.stockProducts.map((product, index) => {
+                        return <Col md={6} lg={4} key={index} style={{ padding: '2px' }}>
+                            <div className='centeredImage'>
+                                <img
+                                    className='productIconSmall'
+                                    alt={`${product.name} ${product.category}`}
+                                    src={window.location.origin + '/' + product.image_url}
+                                />
+                            </div>
+                            <div className='centeredImage'>
+                                <span className='productNameText'>
+                                    {product.name.toLowerCase()}
+                                </span>
+                                <br />
+                                <span className='categoryText'>
+                                    {product.category.toLowerCase()}
+                                </span>
+                            </div>
+                            <div className='productPrice'>
+                                {formatter.format(product.price)}
+                            </div>
+                            <div style={{ textAlign: 'center' }}>
+                                <button
+                                    className='addToCartButton'
+                                    id={`button${index}`}
+                                    onClick={() => this.handleClick(product, index)}
+                                >
+                                    {this.state.buttonTexts[index].toUpperCase()}
+                                </button>
+                            </div>
+                        </Col>
+                    })}
+                </Row>
+                <Modal show={this.state.showModal} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Shopping cart</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body><ShoppingCart /></Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleClose}>
+                            Save Changes
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
         );
     }
 }
